ae9a5f63132d2e85badb0e111e507199bf890e36
jprodriguez60@gmail.com <jprodriguez60@gmail.com>
2018-11-11 12:19:33 -0400

    Added InitialStateFactory for generation of initial state builder by its name

---
+++ tss/src/Builder/InitialStateFactory.java
@@ -0,0 +1,14 @@
+package Builder;
+
+import Modelo.StatesNames;
+
+/**
+ * Created by Jp on 11/11/2018.
+ */
+public class InitialStateFactory {
+
+    public static StateBuilder getInitialState(StatesNames stateName){
+        //TODO: code FELCVBuilder and MinPubBuilder
+        return null;
+    }
+}
