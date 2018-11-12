3d2133096baabb1b73f95028147366ce57ffed18
jprodriguez60@gmail.com <jprodriguez60@gmail.com>
2018-11-12 16:32:28 -0400

    Agregado codigo de prueba en main

--- tss/src/Main.java
+++ tss/src/Main.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import Builder.RandomProcessBuilder;
+import Modelo.Event;
+import Modelo.ProcessBuilder;
+import Modelo.StatesNames;
 
+import java.util.Date;
 
-/**
- *
- * @author Equipo
- */
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String eventDescription="";
+        String eventLocationDescription="";
+        long systemMs=System.currentTimeMillis();
+        Date eventDate=new Date(systemMs);
+        Date eventDelationDate=new Date(systemMs+10000);
+        StatesNames initialState=StatesNames.FELCV;
+        ProcessBuilder processBuilder=new RandomProcessBuilder();
+        Event event=new Event(eventDescription,
+                              eventLocationDescription,
+                              eventDate,
+                              eventDelationDate,
+                              processBuilder,
+                              initialState);
+        System.out.println(event);
     }
     
 }
